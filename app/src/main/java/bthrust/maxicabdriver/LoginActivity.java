90627693d05ff443d7bc9940d4dc714cd43a2fda
mkpmusafir <musaphir2017@gmail.com>
2018-01-23 10:35:16 +0530

    dasfsd
    fdsf

---
+++ app/src/main/java/bthrust/maxicabdriver/LoginActivity.java
@@ -0,0 +1,96 @@
+package bthrust.maxicabdriver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText driverEmail  , driverPass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN , WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_login);
+
+
+         driverEmail = (EditText) findViewById(R.id.driver_email_edit);
+         driverPass = (EditText) findViewById(R.id.driver_pass_edit);
+
+        TextView loginButton = (TextView) findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                loadDriverData(driverEmail.getText().toString() , driverPass.getText().toString());
+            }
+        });
+    }
+
+    private void loadDriverData(final String driver_Email, final String driver_Pass) {
+
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, "URL", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }){
+
+            public Map<String , String> getParams() throws AuthFailureError{
+
+                Map<String , String> params = new HashMap<String  , String>();
+                params.put("" , driver_Email);
+                params.put("" ,driver_Pass);
+                return params;
+            }
+        };
+
+        stringRequest.setRetryPolicy(new RetryPolicy() {
+            @Override
+            public int getCurrentTimeout() {
+                return 40000;
+            }
+
+            @Override
+            public int getCurrentRetryCount() {
+                return 40000;
+            }
+
+            @Override
+            public void retry(VolleyError error) throws VolleyError {
+
+                Toast.makeText(getApplicationContext() , "Slow Network Process..!" , Toast.LENGTH_LONG).show();
+            }
+        });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(LoginActivity.this);
+        requestQueue.add(stringRequest);
+    }
+}
